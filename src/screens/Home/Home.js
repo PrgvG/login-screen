2bbe7933b18a1b318e5505ba3fc7ce8c47287c08
Pirogov German <61702344+PrgvG@users.noreply.github.com>
2022-12-02 20:16:59 +0300

    Test

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,20 +1,23 @@
-import React from "react"
-import CustomButton from "../../components/CustomButton/CustomButton"
-import { ButtonType } from "../Login/utils"
-import useAuth from "../../auth/useAuth"
-import { useNavigate } from "react-router-dom"
-import s from "./Home.module.css"
-import { getTitle, LOREM_TEXT, LOGOUT_TEXT } from "./utils"
+import React from "react";
+import CustomButton from "../../components/CustomButton/CustomButton";
+import { ButtonType } from "../Login/utils";
+import useAuth from "../../auth/useAuth";
+import { useNavigate } from "react-router-dom";
+import s from "./Home.module.css";
+import { getTitle, LOREM_TEXT, LOGOUT_TEXT } from "./utils";
 
 const Home = () => {
-    const navigate = useNavigate()
-    const auth = useAuth()
+    const navigate = useNavigate();
+    const auth = useAuth();
 
     const handleLogout = () => {
-        localStorage.clear()
-        auth.signout(() => navigate("/"))
-    }
-    const title = getTitle(auth.user)
+        localStorage.clear();
+        auth.signout(() => navigate("/"));
+    };
+
+    console.log("test");
+
+    const title = getTitle(auth.user);
     return (
         <section className={s.wrapper}>
             <header className={s.header}>
@@ -23,7 +26,7 @@ const Home = () => {
             </header>
             <section className={s.container}>{LOREM_TEXT}</section>
         </section>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
